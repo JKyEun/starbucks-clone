49f7e3d6f5f3c7437eecb7ee023e5b63ee5977cd
JKyEun <j56237@naver.com>
2023-02-16 15:45:02 +0900

    20230216 JS 기능 마무리(클론 코딩 마무리)

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ const searchSpanEl = searchEl.querySelector("span");
 let isSearchInputFocused = false;
 
 searchSpanEl.addEventListener("click", function () {
-  if (isSearchInputFocused) {
-    searchInputEl.blur();
-    isSearchInputFocused = false;
-  } else {
+  if (!isSearchInputFocused) {
     searchInputEl.focus();
     isSearchInputFocused = true;
+  } else {
+    searchInputEl.blur();
+    isSearchInputFocused = false;
   }
 });
 searchInputEl.addEventListener("focus", function () {
@@ -20,3 +20,107 @@ searchInputEl.addEventListener("focus", function () {
 searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// SWIPER
+// SWIPER NOTICE
+const swiperNotice = new Swiper(
+  ".notice .notice-line .inner .inner-left .swiper",
+  {
+    direction: "vertical",
+    loop: true,
+    autoplay: true,
+  }
+);
+
+//SWIPER PROMOTION
+const swiperPromotion = new Swiper(".notice .promotion .swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  touchRatio: 0,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
+
+const playBtn = document.querySelector("#playBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const playPauseDiv = document.querySelector(".start-stop");
+
+function playPagination() {
+  if (playBtn.classList.contains("hidden")) {
+    playBtn.classList.remove("hidden");
+    pauseBtn.classList.add("hidden");
+    swiperPromotion.autoplay.stop();
+  } else {
+    playBtn.classList.add("hidden");
+    pauseBtn.classList.remove("hidden");
+    swiperPromotion.autoplay.start();
+  }
+}
+
+playPauseDiv.addEventListener("click", playPagination);
+
+// Toggle Promotion
+const promotionSection = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+function changeToggleBtn() {
+  if (promotionSection.classList.contains("hide")) {
+    promotionSection.classList.remove("hide");
+    promotionToggleBtn.classList.add("rotated");
+  } else {
+    promotionSection.classList.add("hide");
+    promotionToggleBtn.classList.remove("rotated");
+  }
+}
+
+promotionToggleBtn.addEventListener("click", changeToggleBtn);
+
+// Animation
+window.onload = function () {
+  const visualSection = document.querySelector(".visual");
+  visualSection.classList.add("animate");
+};
+
+// Scroll
+// Elsalvador
+function showItemIfScroll() {
+  if (window.scrollY > 60) {
+    const elsalvadorSection = document.querySelector(".elsalvador");
+    elsalvadorSection.classList.add("animate");
+  }
+
+  if (window.scrollY > 557) {
+    const ethiopiaSection = document.querySelector(".ethiopia");
+    ethiopiaSection.classList.add("animate");
+  }
+
+  if (window.scrollY > 950) {
+    const favoriteSection = document.querySelector(".favorite");
+    favoriteSection.classList.add("animate");
+  }
+
+  if (window.scrollY > 1765) {
+    const magazineSection = document.querySelector(".magazine");
+    magazineSection.classList.add("animate");
+  }
+
+  if (window.scrollY > 2153) {
+    const findStoreSection = document.querySelector(".find-store");
+    findStoreSection.classList.add("animate");
+  }
+}
+
+window.addEventListener("scroll", showItemIfScroll)
