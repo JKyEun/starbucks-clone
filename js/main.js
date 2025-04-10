fcda2859fa57149f9b62b4c9cca6d5b310716454
JKyEun <j56237@naver.com>
2025-04-10 14:22:08 +0900

    feat: 스크롤 애니메이션 로직 개선

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // HEADER
 // SEARCH
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-const searchSpanEl = searchEl.querySelector("span");
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+const searchSpanEl = searchEl.querySelector('span');
 let isSearchInputFocused = false;
 
-searchSpanEl.addEventListener("click", function () {
+searchSpanEl.addEventListener('click', function () {
   if (!isSearchInputFocused) {
     searchInputEl.focus();
     isSearchInputFocused = true;
@@ -14,27 +14,24 @@ searchSpanEl.addEventListener("click", function () {
     isSearchInputFocused = false;
   }
 });
-searchInputEl.addEventListener("focus", function () {
-  searchInputEl.setAttribute("placeholder", "통합 검색");
+searchInputEl.addEventListener('focus', function () {
+  searchInputEl.setAttribute('placeholder', '통합 검색');
 });
-searchInputEl.addEventListener("blur", function () {
-  searchInputEl.setAttribute("placeholder", "");
+searchInputEl.addEventListener('blur', function () {
+  searchInputEl.setAttribute('placeholder', '');
 });
 
 // SWIPER
 // SWIPER NOTICE
-const swiperNotice = new Swiper(
-  ".notice .notice-line .inner .inner-left .swiper",
-  {
-    direction: "vertical",
-    loop: true,
-    autoplay: true,
-  }
-);
+const swiperNotice = new Swiper('.notice .notice-line .inner .inner-left .swiper', {
+  direction: 'vertical',
+  loop: true,
+  autoplay: true
+});
 
 //SWIPER PROMOTION
-const swiperPromotion = new Swiper(".notice .promotion .swiper", {
-  direction: "horizontal",
+const swiperPromotion = new Swiper('.notice .promotion .swiper', {
+  direction: 'horizontal',
   slidesPerView: 3,
   spaceBetween: 10,
   centeredSlides: true,
@@ -42,86 +39,79 @@ const swiperPromotion = new Swiper(".notice .promotion .swiper", {
   touchRatio: 0,
   autoplay: {
     delay: 2000,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   pagination: {
-    el: ".promotion .swiper-pagination",
-    clickable: true,
+    el: '.promotion .swiper-pagination',
+    clickable: true
   },
   navigation: {
-    prevEl: ".swiper-button-prev",
-    nextEl: ".swiper-button-next",
-  },
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next'
+  }
 });
 
-const playBtn = document.querySelector("#playBtn");
-const pauseBtn = document.querySelector("#pauseBtn");
-const playPauseDiv = document.querySelector(".start-stop");
+const playBtn = document.querySelector('#playBtn');
+const pauseBtn = document.querySelector('#pauseBtn');
+const playPauseDiv = document.querySelector('.start-stop');
 
 function playPagination() {
-  if (playBtn.classList.contains("hidden")) {
-    playBtn.classList.remove("hidden");
-    pauseBtn.classList.add("hidden");
+  if (playBtn.classList.contains('hidden')) {
+    playBtn.classList.remove('hidden');
+    pauseBtn.classList.add('hidden');
     swiperPromotion.autoplay.stop();
   } else {
-    playBtn.classList.add("hidden");
-    pauseBtn.classList.remove("hidden");
+    playBtn.classList.add('hidden');
+    pauseBtn.classList.remove('hidden');
     swiperPromotion.autoplay.start();
   }
 }
 
-playPauseDiv.addEventListener("click", playPagination);
+playPauseDiv.addEventListener('click', playPagination);
 
 // Toggle Promotion
-const promotionSection = document.querySelector(".promotion");
-const promotionToggleBtn = document.querySelector(".toggle-promotion");
+const promotionSection = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
 
 function changeToggleBtn() {
-  if (promotionSection.classList.contains("hide")) {
-    promotionSection.classList.remove("hide");
-    promotionToggleBtn.classList.add("rotated");
+  if (promotionSection.classList.contains('hide')) {
+    promotionSection.classList.remove('hide');
+    promotionToggleBtn.classList.add('rotated');
   } else {
-    promotionSection.classList.add("hide");
-    promotionToggleBtn.classList.remove("rotated");
+    promotionSection.classList.add('hide');
+    promotionToggleBtn.classList.remove('rotated');
   }
 }
 
-promotionToggleBtn.addEventListener("click", changeToggleBtn);
+promotionToggleBtn.addEventListener('click', changeToggleBtn);
 
 // Animation
 window.onload = function () {
-  const visualSection = document.querySelector(".visual");
-  visualSection.classList.add("animate");
+  const visualSection = document.querySelector('.visual');
+  visualSection.classList.add('animate');
 };
 
 // Scroll
 // Elsalvador
 // 현재 스크롤 위치에 따라 animate 클래스를 넣어 animation 효과를 주는 함수
 function showItemIfScroll() {
-  if (window.scrollY > 60) {
-    const elsalvadorSection = document.querySelector(".elsalvador");
-    elsalvadorSection.classList.add("animate");
-  }
+  const sections = [
+    { selector: '.elsalvador', offset: 0.1 },
+    { selector: '.ethiopia', offset: 0.3 },
+    { selector: '.favorite', offset: 0.5 },
+    { selector: '.magazine', offset: 0.7 },
+    { selector: '.find-store', offset: 0.9 }
+  ];
 
-  if (window.scrollY > 600) {
-    const ethiopiaSection = document.querySelector(".ethiopia");
-    ethiopiaSection.classList.add("animate");
-  }
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight - windowHeight;
 
-  if (window.scrollY > 1000) {
-    const favoriteSection = document.querySelector(".favorite");
-    favoriteSection.classList.add("animate");
-  }
-
-  if (window.scrollY > 2000) {
-    const magazineSection = document.querySelector(".magazine");
-    magazineSection.classList.add("animate");
-  }
-
-  if (window.scrollY > 2300) {
-    const findStoreSection = document.querySelector(".find-store");
-    findStoreSection.classList.add("animate");
-  }
+  sections.forEach(({ selector, offset }) => {
+    const section = document.querySelector(selector);
+    if (section && window.scrollY > documentHeight * offset) {
+      section.classList.add('animate');
+    }
+  });
 }
 
-window.addEventListener("scroll", showItemIfScroll)
+window.addEventListener('scroll', showItemIfScroll);
